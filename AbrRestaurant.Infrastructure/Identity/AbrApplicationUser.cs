04912e7800715a06bd14251330ce7e2011479f7a
Iskander Raimbayev <iskander.raimbayev@outlook.com>
2021-05-11 20:39:12 +0600

    Minor fixes + added Username property on AbrApplicationUser

--- AbrRestaurant.Infrastructure/Identity/AbrApplicationUser.cs
+++ AbrRestaurant.Infrastructure/Identity/AbrApplicationUser.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace AbrRestaurant.Infrastructure.Identity
 {
     public class AbrApplicationUser : IdentityUser
     {
+        [Required]
+        public string Username { get; set; }
         public int ? LastSignOutMomentTimestamp { get; set; }
     }
 }
---
+++ AbrRestaurant.MenuApi/Contracts/Shared/ApiErrorResponse.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace AbrRestaurant.MenuApi.Contracts.Shared
+{
+    public class ApiErrorResponse<T>
+    {
+        [JsonProperty("trace_id")]
+        public string TraceId { get; set; }
+
+        [JsonProperty("moment_utc")]
+        public string MomentUtc { get; set; }
+
+        [JsonProperty("error_descriptions")]
+        public IEnumerable<string> ErrorDescriptions { get; set; }
+
+        [JsonProperty("http_status_code")]
+        public int HttpStatusCode { get; set; }
+
+        [JsonProperty("error_object", NullValueHandling = NullValueHandling.Ignore)]
+        public T ErrorObject { get; set; }
+    }
+}
